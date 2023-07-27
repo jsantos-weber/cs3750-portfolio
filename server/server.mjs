22eecf52dbfb478ffd200cbf5a4e2af8b528aed2
Junsiwoo45 <haydenbackwards@gmail.com>
2023-07-26 18:26:53 -0600

    testpush
    test

--- server/server.mjs
+++ server/server.mjs
@@ -92,7 +92,7 @@ function generateDeck() {
     }
   }
 
-  // Shuffle the deck using Fisher-Yates shuffle
+  // Shuffle the deck 
   for (let i = deck.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [deck[i], deck[j]] = [deck[j], deck[i]];
