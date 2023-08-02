fa22665e2f15267bdbd793c51e6b510c28a5f61c
Sione Havili <sionehavili2@gmail.com>
2023-08-02 17:07:40 -0600

    Update App.js

--- speed/src/App.js
+++ speed/src/App.js
@@ -8,7 +8,6 @@ import HelloWorld from "./components/HelloWorld";
 import SpeedNavbar from "./components/SpeedNavbar";
 import Deck from "./components/Deck";
 import Emotes from "./components/Emotes/Emotes";
-import Chat from "./components/Chat.js";
 import { Gamelobby } from "./components/Gamelobby";
 import MessageComponent from "./components/MessageComponent";
 
