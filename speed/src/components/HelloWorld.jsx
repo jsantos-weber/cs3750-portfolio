d5ee660a30d0972090930c046c072a8b6cf8178a
Junsiwoo45 <haydenbackwards@gmail.com>
2023-08-02 13:02:33 -0600

    Timer

--- speed/src/components/HelloWorld.jsx
+++ speed/src/components/HelloWorld.jsx
@@ -1,19 +1,43 @@
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useState } from "react";
 import Emotes from "./Emotes/Emotes";
 
 const HelloWorld = () => {
   const [display, setDisplay] = useState(false);
   const [message, setMessage] = useState("");
+  const [timer, setTimer] = useState(3);
+  const [showTimer, setShowTimer] = useState(false);
+
   let hello_message = "Hello World!";
+
+  useEffect(() => {
+    if (showTimer && timer > 0) {
+      const countdownInterval = setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
+
+      return () => {
+        clearInterval(countdownInterval);
+      };
+    } else if (timer === 0 && showTimer) {
+      setShowTimer(false);
+      setMessage("Go!");
+      setDisplay(true);
+    }
+  }, [showTimer, timer]);
+
+  const handleStartTimer = () => {
+    setShowTimer(true);
+    setTimer(3);
+    setDisplay(false);
+    setMessage("");
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const message = {
       message: hello_message,
     };
-
-   
 
     axios
       .post("http://localhost:4000/hello", {
@@ -29,13 +53,16 @@ const HelloWorld = () => {
       });
   };
 
-
-
-  
   return (
     <div>
-      <button onClick={handleSubmit}>Click Me</button>
+      {showTimer ? <p>{timer > 0 ? timer : "Go"}</p> : null}
+
+      <button onClick={handleStartTimer} disabled={showTimer}>
+        {showTimer ? "Wait..." : "Click Me"}
+      </button>
+
       <Emotes />
+
       {display && <p>{message}</p>}
     </div>
   );
